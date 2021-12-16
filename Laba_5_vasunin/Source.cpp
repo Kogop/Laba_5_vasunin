2c0330c79aafd67b536a90a9c1658aaa117d619b
Nesterov Vlad <nesterovvlad100@gmail.com>
2021-12-16 13:07:46 +0300

    q

--- Laba_5_vasunin/Source.cpp
+++ Laba_5_vasunin/Source.cpp
@@ -5,15 +5,11 @@ using namespace std;
 double n = 0;
 double res = 0;
 
-//double Factorial(double n) {
-//	return (n == 0) ? 1 : n * Factorial(n - 1);
-//};
 
 int main() {
 
 	cin >> n;
-	//cout << endl;
-	//double m = Factorial(n);
+	
 	double m = 0;
 	int i = 1;
 	while (n >= pow(5,i))
